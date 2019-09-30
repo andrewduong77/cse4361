449136651ea7a010c7587e43910862df482acad7
Andrew Duong <andrewduong77@yahoo.com>
2019-09-29 21:50:17 -0500

    still working, correct p1 to p2 for setBackground()

--- Homework/Homework 1/test.java
+++ Homework/Homework 1/test.java
@@ -19,7 +19,6 @@ class test extends JFrame {
  // main class 
  public static void main(String[] args) 
  { 
-	 System.out.println("Hello world");
      // create a new frame to stor text field and button 
      f = new JFrame("panel"); 
      mainPanel = new JPanel();
@@ -31,17 +30,17 @@ class test extends JFrame {
 
      // create a panel to add buttons 
      JPanel p1 = new JPanel(); 
-     //p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
+     p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
      p1.setBackground(Color.DARK_GRAY);
      JPanel p2 = new JPanel();
-     //p2.setLayout(new BoxLayout(p2, BoxLayout.Y_AXIS));
+     p2.setLayout(new BoxLayout(p2, BoxLayout.Y_AXIS));
+     p2.setBackground(Color.red); 
 
      // add buttons and textfield to panel 
      p1.add(b); 
      p1.add(b1); 
 
      // setbackground of panel 
-     p1.setBackground(Color.red); 
 
      // add panel to frame 
      mainPanel.add(p1); 
