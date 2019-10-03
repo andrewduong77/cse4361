c942be36b148f9ba6c82650e66502ca3002c13d4
Andrew Duong <andrewduong77@yahoo.com>
2019-10-03 18:06:57 -0500

    code cleanup

--- Homework/Homework 1/test.java
+++ Homework/Homework 1/test.java
@@ -10,44 +10,36 @@ class test extends JFrame {
 
     private static final long serialVersionUID = 1L;
 
-    // JFrame 
-    static JFrame f;
-    static JPanel mainPanel;
-
-    // JButton 
-    static JButton b, b1; 
-
     // main class 
     public static void main(String[] args) 
     { 
         // create a new frame to stor text field and button 
-        f = new JFrame("HW1 GUI"); 
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+        JFrame frame = new JFrame("HW1 GUI");
 
-        // create a new buttons 
-        b = new JButton("Circle"); 
-        b1 = new JButton("Box");
+        // create a new buttons
+        // JButton button_circle, button_box;
+        JButton button_circle = new JButton("Circle");
+        JButton button_box = new JButton("Box");
 
         // create a panel to add buttons 
-        JPanel p1 = new JPanel(); 
-        p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
-        p1.setBackground(Color.DARK_GRAY);
+        JPanel panel_left = new JPanel(); 
+        panel_left.setLayout(new BoxLayout(panel_left, BoxLayout.Y_AXIS));
+        panel_left.setBackground(Color.DARK_GRAY);
      
-        JPanel p2 = new JPanel();
-        p2.setBackground(Color.white); 
+        JPanel panel_right = new JPanel();
+        panel_right.setBackground(Color.white); 
 
         // add buttons and textfield to panel 
-        p1.add(b,BorderLayout.WEST); 
-        p1.add(b1,BorderLayout.WEST); 
+        panel_left.add(button_circle,BorderLayout.WEST); 
+        panel_left.add(button_box,BorderLayout.WEST); 
 
         // add panel to frame 
-        f.add(p1,BorderLayout.WEST);
-        f.add(p2,BorderLayout.CENTER);
+        frame.add(panel_left,BorderLayout.WEST);
+        frame.add(panel_right,BorderLayout.CENTER);
 
         // set the size of frame 
-        f.setSize(1500, 1500); 
-        f.setVisible(true);
-     
+        frame.setSize(600, 400); 
+        frame.setVisible(true);
+
     } 
 } 
