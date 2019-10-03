c9c72f3e1688f5f6fcb53198382a3208ed219659
Andrew Duong <andrewduong77@yahoo.com>
2019-10-03 17:45:12 -0500

    fixed identations

--- Homework/Homework 1/test.java
+++ Homework/Homework 1/test.java
@@ -8,46 +8,46 @@ import javax.swing.JPanel;
 
 class test extends JFrame { 
 
- private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-// JFrame 
- static JFrame f;
- static JPanel mainPanel;
+    // JFrame 
+    static JFrame f;
+    static JPanel mainPanel;
 
- // JButton 
- static JButton b, b1; 
+    // JButton 
+    static JButton b, b1; 
 
- // main class 
- public static void main(String[] args) 
- { 
-     // create a new frame to stor text field and button 
-     f = new JFrame("HW1 GUI"); 
-     mainPanel = new JPanel();
-     mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+    // main class 
+    public static void main(String[] args) 
+    { 
+        // create a new frame to stor text field and button 
+        f = new JFrame("HW1 GUI"); 
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 
-     // create a new buttons 
-     b = new JButton("Circle"); 
-     b1 = new JButton("Box");
+        // create a new buttons 
+        b = new JButton("Circle"); 
+        b1 = new JButton("Box");
 
-     // create a panel to add buttons 
-     JPanel p1 = new JPanel(); 
-     p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
-     p1.setBackground(Color.DARK_GRAY);
+        // create a panel to add buttons 
+        JPanel p1 = new JPanel(); 
+        p1.setLayout(new BoxLayout(p1, BoxLayout.Y_AXIS));
+        p1.setBackground(Color.DARK_GRAY);
      
-     JPanel p2 = new JPanel();
-     p2.setBackground(Color.white); 
+        JPanel p2 = new JPanel();
+        p2.setBackground(Color.white); 
 
-     // add buttons and textfield to panel 
-     p1.add(b,BorderLayout.WEST); 
-     p1.add(b1,BorderLayout.WEST); 
+        // add buttons and textfield to panel 
+        p1.add(b,BorderLayout.WEST); 
+        p1.add(b1,BorderLayout.WEST); 
 
-     // add panel to frame 
-     f.add(p1,BorderLayout.WEST);
-     f.add(p2,BorderLayout.CENTER);
+        // add panel to frame 
+        f.add(p1,BorderLayout.WEST);
+        f.add(p2,BorderLayout.CENTER);
 
-     // set the size of frame 
-     f.setSize(1500, 1500); 
-     f.setVisible(true);
+        // set the size of frame 
+        f.setSize(1500, 1500); 
+        f.setVisible(true);
      
- } 
+    } 
 } 
