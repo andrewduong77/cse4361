8591917effedd8af1e316db459b1be3b9f3c8bdf
Andrew Duong <andrewduong77@yahoo.com>
2019-10-03 18:41:08 -0500

    adding ActionListener

--- Homework/Homework 1/test.java
+++ Homework/Homework 1/test.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -11,35 +12,44 @@ class test extends JFrame {
     private static final long serialVersionUID = 1L;
 
     // main class 
-    public static void main(String[] args) 
-    { 
+    public static void main(String[] args) { 
         // create a new frame to stor text field and button 
         JFrame frame = new JFrame("HW1 GUI");
 
         // create a new buttons
-        // JButton button_circle, button_box;
-        JButton button_circle = new JButton("Circle");
-        JButton button_box = new JButton("Box");
+        // JButton buttonCircle, buttonBox;
+        JButton buttonCircle = new JButton("Circle");
+        JButton buttonBox = new JButton("Box");
 
         // create a panel to add buttons 
-        JPanel panel_left = new JPanel(); 
-        panel_left.setLayout(new BoxLayout(panel_left, BoxLayout.Y_AXIS));
-        panel_left.setBackground(Color.DARK_GRAY);
+        JPanel panelLeft = new JPanel(); 
+        panelLeft.setLayout(new BoxLayout(panelLeft, BoxLayout.Y_AXIS));
+        panelLeft.setBackground(Color.DARK_GRAY);
      
-        JPanel panel_right = new JPanel();
-        panel_right.setBackground(Color.white); 
+        JPanel panelRight = new JPanel();
+        panelRight.setBackground(Color.white); 
 
         // add buttons and textfield to panel 
-        panel_left.add(button_circle,BorderLayout.WEST); 
-        panel_left.add(button_box,BorderLayout.WEST); 
+        panelLeft.add(buttonCircle,BorderLayout.WEST); 
+        panelLeft.add(buttonBox,BorderLayout.WEST); 
 
         // add panel to frame 
-        frame.add(panel_left,BorderLayout.WEST);
-        frame.add(panel_right,BorderLayout.CENTER);
+        frame.add(panelLeft,BorderLayout.WEST);
+        frame.add(panelRight,BorderLayout.CENTER);
 
         // set the size of frame 
         frame.setSize(600, 400); 
         frame.setVisible(true);
 
-    } 
+        buttonCircle.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buttonBox.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+            
+            }
+        });
+    }
 } 
