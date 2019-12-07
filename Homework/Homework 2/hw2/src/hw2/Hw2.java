139a474d88d863f2a39fff7dc19a13388dcaec11
andrewduong77 <36346764+andrewduong77@users.noreply.github.com>
2019-12-06 18:50:30 -0600

    removed duplicated frame in both hw2 and hw4

--- Homework/Homework 2/hw2/src/hw2/Hw2.java
+++ Homework/Homework 2/hw2/src/hw2/Hw2.java
@@ -5,6 +5,5 @@ public class Hw2
     public static void main(String[] args)
     {
         View view = new View();
-        Controller controller = new Controller(view);
     }
 }
--- Homework/Homework 4/hw4/src/hw4/Hw4.java
+++ Homework/Homework 4/hw4/src/hw4/Hw4.java
@@ -5,6 +5,5 @@ public class Hw4
     public static void main(String[] args)
     {
         View view = new View();
-        Controller controller = new Controller(view);
     }
 }
